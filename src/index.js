825dfb8dd58b23e13ae833da42463af3130b4f4c
Ivan Prodeus <learning.webpractice@gmail.com>
2022-09-04 22:27:04 +0300

    task 10 in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputField = document.querySelector('#search-box');
+
+
+
+inputField.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(event){
+    const inputText = event.target.value;
+    if(inputText === ""){
+        return
+    }
+    fetchCountries(inputText);
+};
+
