af20fc7c43ba9571d7f7b302e4dd4e7d71184402
Ivan Prodeus <learning.webpractice@gmail.com>
2022-09-06 19:02:09 +0300

    reformatted structure

--- src/index.js
+++ src/index.js
@@ -21,56 +21,57 @@ function onInput(event){
     
     fetchCountries(inputText.trim()).then(countries => checkingQuantityInAnswer(countries))
     .catch(error =>Notify.failure('Oops, there is no country with that name'));
-    
-    function checkingQuantityInAnswer(reply){
-        if(reply.length > 10){
-            Notify.info("Too many matches found. Please enter a more specific name.");
-            return;
-        } else if(reply.length === 1) {
-            clearDysplay()
-            displayMarkup(reply);
-        } else {
-            displayListMarkup(reply);
-        }
-    };
-    
-    function displayListMarkup(datas){
-        const markup = datas.map(country => {
-            return `<li class="country-list_item">
-              <img src="${country.flags.svg}" alt="${country.name.official}" width="50" heigth ='50'>
-                <b class="countryName">${country.name.official}</b>
-              </li>`;
-          }).join('');
-    
-        countryList.innerHTML = markup;
-    };
-    
-    function displayMarkup(data){
-        const markup = data.map(country => {
-            const language = countryLanguage(country.languages);
-            return `<div class="header-block">
-            <img src="${country.flags.svg}" alt="flag of ${country.name.common}" class="header-block_img" width="100">
-            <h1 class="header-block_text">${country.name.common}</h1>
-          </div>
-          <p><b>Name official</b>: ${country.name.official}</p>
-          <p><b>Name</b>: ${country.name.common}</p>
-          <p><b>Capital</b>: ${country.capital}</p>
-          <p><b>Languages</b>: ${language}</p>
-          <p><b>Population</b>: ${country.population}</p>
-          `;
-          }).join('');
-        countryInfo.innerHTML = markup;
-    };
 
-    
-    function countryLanguage(languages) {
-        const langs = [];
-        for (let key in languages) {
-            langs.push(languages[key]);
-        }
-        return langs;
+};
+
+function checkingQuantityInAnswer(reply){
+    if(reply.length > 10){
+        Notify.info("Too many matches found. Please enter a more specific name.");
+        return;
+    } else if(reply.length === 1) {
+        clearDysplay()
+        displayMarkup(reply);
+    } else {
+        displayListMarkup(reply);
     }
 };
+
+function displayListMarkup(datas){
+    const markup = datas.map(country => {
+        return `<li class="country-list_item">
+            <img src="${country.flags.svg}" alt="${country.name.official}" width="50" heigth ='50'>
+            <b class="countryName">${country.name.official}</b>
+            </li>`;
+    }).join('');
+
+    countryList.innerHTML = markup;
+};
+
+function displayMarkup(data){
+    const markup = data.map(country => {
+        const language = countryLanguage(country.languages);
+        return `<div class="header-block">
+        <img src="${country.flags.svg}" alt="flag of ${country.name.common}" class="header-block_img" width="100">
+        <h1 class="header-block_text">${country.name.common}</h1>
+        </div>
+        <p><b>Name official</b>: ${country.name.official}</p>
+        <p><b>Name</b>: ${country.name.common}</p>
+        <p><b>Capital</b>: ${country.capital}</p>
+        <p><b>Languages</b>: ${language}</p>
+        <p><b>Population</b>: ${country.population}</p>
+        `;
+    }).join('');
+    countryInfo.innerHTML = markup;
+};
+
+function countryLanguage(languages) {
+    const langs = [];
+    for (let key in languages) {
+        langs.push(languages[key]);
+    }
+    return langs;
+}
+
 function clearDysplay(){
     countryList.innerHTML = "";
     countryInfo.innerHTML = "";
